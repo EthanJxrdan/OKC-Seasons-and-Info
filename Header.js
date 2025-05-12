119ee2746c9a7f112a9731d290f5d3c2464219be
Ethan Jordan <ethan07jordan@gmail.com>
2025-05-12 13:19:01 -0500

    Add files via upload

---
+++ Header.js
@@ -0,0 +1,18 @@
+function Header() {
+  return (
+    <nav class="navbar bg-dark border-bottom border-body" data-bs-theme="dark">
+      <div class="container justify-content-center">
+        <h1 className="display-1 fs-2 navbar-brand text-light">Player Bios</h1>
+        <ul className="nav nav-underline">
+          <li className="nav-item">
+            <a href="..\docs\index.html" className="nav-link text-light">
+              Home
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default Header;
---
+++ Player.js
@@ -0,0 +1,5 @@
+function Player(props) {
+  return <h3>Player #{props.id}</h3>;
+}
+
+export default Player;
